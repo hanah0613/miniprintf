fbdde34550903fb199ce3119c33dcc7c6d8217ca
hanah0613 <qogksk777@gmail.com>
2018-02-13 15:59:59 +0900

    complete two lines

--- test/mini_.c
+++ test/mini_.c
@@ -5,9 +5,9 @@ int main()
 {
     mini_printf ("hello, world\n");
     mini_printf ("[%99% %T %d]\n", 20); 
-#if 0
     mini_printf ("[%d %x %X %b]\n", 15, 15, 15, 15); 
     mini_printf ("[%+010ld]\n", 999l); 
+#if 0
     mini_printf ("[%+010.005ld]\n", -999l);
     mini_printf ("[%10.6b]\n", 16);
     mini_printf ("[%.6s]\n", "hello, world");
